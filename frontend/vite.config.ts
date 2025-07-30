793bad127202efda10fce8f749df2202d96cdd07
MishaNavrotsky <mi.navrotskyy@gmail.com>
2025-07-30 23:24:41 +0300

    Setup for react-router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    strictPort: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    }
+  }
+  
 });
