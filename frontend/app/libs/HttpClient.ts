fbe2441ca7566255b10a9f8e233f340e6cb23b42
MishaNavrotsky <mi.navrotskyy@gmail.com>
2025-08-07 07:14:26 +0300

    -

--- frontend/app/libs/HttpClient.ts
+++ frontend/app/libs/HttpClient.ts
@@ -1,7 +1,11 @@
-import axios from "axios";
+import axios, { type InternalAxiosRequestConfig } from "axios";
 import { BACKEND_URL } from "~/env";
 
 export const Backend = axios.create({
     baseURL: BACKEND_URL,
     withCredentials: true,
 });
+
+Backend.interceptors.request.use((config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    return config;
+})
